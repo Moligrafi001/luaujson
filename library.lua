fee4b305e502410603038e9d83eb68b77ed3c850
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2025-05-19 23:58:42 -0300

    update library.lua

--- library.lua
+++ library.lua
@@ -1,3 +1,45 @@
 local luaujson = {}
 
+-- Local Functions
+local function AdmMsg(type, msg)
+  if type == 1 then
+    print("[ luaujson ] - " .. tostring(msg))
+  elseif type == 2 then
+    warn("[ luaujson ] - " .. tostring(msg))
+  elseif type == 3 then
+    error("[ luaujson ] - " .. tostring(msg))
+  elseif type == 9 then
+    if msg == 1 then
+      print(".*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.")
+    elseif msg == 2 then
+      warn(".*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.")
+    elseif msg == 3 then
+      error(".*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.")
+    end
+  end
+end
+
+-- Public Functions
+function luaujson:Start()
+  AdmMsg(9, 1)
+  AdmMsg(1, "Library started successfully")
+  AdmMsg(9, 1)
+end
+function luaujson:Parse(req, res)
+  if req.Url then
+    req = request(req)
+    if req.Sucess then
+      
+    else
+      AdmMsg(3, "Request failed: " .. req.Url .. " | " .. req.StatusCode)
+    end
+  else
+    AdmMsg(3, "Parse function missing URL param")
+  end
+
+  Parsed = game:GetService("HttpService"):JSONDecode(response.Body)["script"]
+  return
+end
+
+-- Export Library
 return luaujson
